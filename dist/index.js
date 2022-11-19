c2716e7bdc04e4d9047567b9a007f6453ab5c550
Jommel Dalisaymo <dalisaymo36@gmail.com>
2022-11-19 23:09:26 +0800

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+var fontawesome = require('@fortawesome/fontawesome-free');
+var animate = require('animate');
+var aos = require('aos');
+var bootstrap = require('bootstrap');
+var fa = require('font-awesome');
+var im = requrie('include-media');
+var jquery = require('jquery');
